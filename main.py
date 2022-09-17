e4df524c49c04c1b4cb53f8770bb1bd7386ee743
zlj-max <63718784+zlj-max@users.noreply.github.com>
2022-09-17 18:22:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ def get_color():
 
 def get_access_token():
     # appId
-    app_id = config["APP_ID"]
+    app_id = config["app_id"]
     # appSecret
-    app_secret = config["APP_SECRET"]
+    app_secret = config["app_secret"]
     post_url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}"
                 .format(app_id, app_secret))
     try:
-        access_token = get(post_url).json()['TEMPLATE_ID']
+        access_token = get(post_url).json()['access_token']
     except KeyError:
         print("获取access_token失败，请检查app_id和app_secret是否正确")
         os.system("pause")
